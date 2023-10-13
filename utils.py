59e09ba8054276a6701f4a0b8ff1f85530bf57f4
Daisy Abbott <dabbott2117@gmail.com>
2023-10-12 17:34:48 -0700

    removed dls function

--- utils.py
+++ utils.py
@@ -17,7 +17,8 @@ def y_from_filename(rotation_threshold: float, filename: str) -> str:
         return "right"
     else:
         return "forward"
-    
+
+
 def y_from_filename(rotation_threshold, filename) -> str:
     """Extracts the direction label from the filename of an image.
 
@@ -42,26 +43,26 @@ def wandb_load_learner(pth_file: str):
     return model
 
 
-# create data loaders
-def get_dls(args: Namespace, data_path: str):
-    # NOTE: not allowed to add a type annotation to the input
+# # create data loaders
+# def get_dls(args: Namespace, data_path: str):
+#     # NOTE: not allowed to add a type annotation to the input
 
-    image_filenames: list = get_image_files(data_path)  # type:ignore
+#     image_filenames: list = get_image_files(data_path)  # type:ignore
 
-    # Using a partial function to set the rotation_threshold from args
-    label_func = partial(y_from_filename, args.rotation_threshold)
+#     # Using a partial function to set the rotation_threshold from args
+#     label_func = partial(y_from_filename, args.rotation_threshold)
 
-    if args.use_command_image:
-        return get_image_command_category_dataloaders(
-            args, data_path, image_filenames, label_func
-        )
-    else:
-        return ImageDataLoaders.from_name_func(
-            data_path,
-            image_filenames,
-            label_func,
-            valid_pct=args.valid_pct,
-            shuffle=True,
-            bs=args.batch_size,
-            # item_tfms=Resize(224)
-        )
+#     if args.use_command_image:
+#         return get_image_command_category_dataloaders(
+#             args, data_path, image_filenames, label_func
+#         )
+#     else:
+#         return ImageDataLoaders.from_name_func(
+#             data_path,
+#             image_filenames,
+#             label_func,
+#             valid_pct=args.valid_pct,
+#             shuffle=True,
+#             bs=args.batch_size,
+#             # item_tfms=Resize(224)
+#         )
